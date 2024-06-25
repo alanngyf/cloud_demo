bcb3fa0b5fdd14d83e387e0288c219d0d8d5f98f
Alan Ng <alanng@berkeley.edu>
2024-06-24 20:55:58 -0500

    configure and create EurekaServer

---
+++ springcloud-eureka/src/main/java/com/gbacoder/springcloud/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.gbacoder.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author alanulog
+ * @create 6/24/2024 8:51 PM
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
